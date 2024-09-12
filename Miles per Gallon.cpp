47a4ebe9e021562a5c7e7dff1526f64cab3a4c06
b2all <b2all@CAPSULECORP>
2024-09-11 20:30:47 -0400

    Add project files.

---
+++ Miles per Gallon.cpp
@@ -0,0 +1,18 @@
+// Titel: Miles per Gallon.cpp 
+// Name: Yochanan Allen
+// Date: 09/11/24
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+const double gallons = 15.0;
+const double miles = 375.0;
+
+double mpg = miles / gallons;
+
+cout << "The car gets " << mpg << " miles per gallon." << endl;
+
+    return 0;
+}
